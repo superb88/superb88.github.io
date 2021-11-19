6c8845912e11389b8305ce71897dfe6e755adca9
superb88 <94148747+superb88@users.noreply.github.com>
2021-11-19 17:15:57 +0700

    Update and rename script2.js to script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+    // memanggil css pada js
+    var cssId = 'myCss'; 
+    if (!document.getElementById(cssId))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        
+		
+        link.href = 'https://superb88.github.io/style.css';
+        link.media = 'all';
+        head.appendChild(link);
+    }
+
+
+// membuat modal pada js
+    var inimodal = document.createElement("DIV");
+    inimodal.innerHTML = `  <div class="popbox hide" id="popbox">
+                                <div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/>
+                                <div class="pop-content">
+                                    <div class="popcontent" align="center">
+                                        <img data-src="https://1.bp.blogspot.com/-y8AsxfEerDc/YFSyMPZF14I/AAAAAAAAAAM/JUegMgSE-3o5A_06mx0Fir2-dkB6fAGvACLcBGAsYHQ/s640/re.jpg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="  width="640" height="320" class="lazyload ads-img" alt="" />
+                                        <button class='g_url btn btn-success btn-dwn m-2'>Confirm</button>
+                                        <br/>
+                                    </div>
+                                    <button class='g_url popbox-close-button'>&times;</button>
+                                </div>
+                            </div>`;
+    document.body.appendChild(inimodal);
+    
+
+
+// fungsi ketika page di scroll akan memunculkan popup modal
+    var current     = window.location.href;
+    var g_confirm   = current.includes('c=1');
+    var go_ads      = 'https://bit.ly/creativefabricas';
+
+    $(document).ready(function () {
+        if (!g_confirm && !go_ads.includes('#YOUR')) {
+            $(window).scroll(function (event) {
+                var scroll = $(window).scrollTop();
+                if (scroll >= 200) {
+                    $('#popbox').removeClass('hide');
+                }
+                console.log('scroll..'); 
+                
+            });
+        }
+
+
+
+
+        $(document).on('click', '.g_url', function (e) {
+            e.preventDefault();
+
+            var g_target = current.includes("?") ? current + "&c=1" : current + "?c=1";
+
+            window.open(go_ads, "_blank");
+            window.location.href = g_target;
+        });
+
+        $("[id*='google-cache']").remove();
+
+
+        $(document).on('click', '.ads-img', function (e) {
+            e.preventDefault();
+            window.open(go_ads, '_parent');
+            //opsional
+            modal.style.display = "none";
+        });
+    });
